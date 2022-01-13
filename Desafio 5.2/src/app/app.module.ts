211dec73f9cdd20054588a2eb2c4c61564bb69f7
Sergioricmau <94474174+Sergioricmau@users.noreply.github.com>
2022-01-13 15:27:42 -0300

    Add files via upload

---
+++ Desafio 5.2/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentePaiComponent } from './components/componente-pai/componente-pai.component';
+import { ComponenteFilhoComponent } from './components/componente-filho/componente-filho.component';
+import { ListaNumerosComponent } from './components/lista-numeros/lista-numeros.component';
+import { QuadradoComponent } from './components/quadrado/quadrado.component';
+import { CuboComponent } from './components/cubo/cubo.component';
+import { TestesPipeComponent } from './components/testes-pipe/testes-pipe.component';
+import { CodigoConvitePipe } from './components/pipes/codigo-convite.pipe';
+import { CpfCnpjPipe } from './components/pipes/cpf-cnpj.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentePaiComponent,
+    ComponenteFilhoComponent,
+    ListaNumerosComponent,
+    QuadradoComponent,
+    CuboComponent,
+    TestesPipeComponent,
+    CodigoConvitePipe,
+    CpfCnpjPipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
